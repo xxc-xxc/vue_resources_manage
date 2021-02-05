9cce6b6f9f901ec0b7719df8a9f00700d4a06285
xxc <2331896806@qq.com>
2021-02-05 13:40:48 +0800

    添加登录组件

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login_container">
+        登录组件
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.login_container {
+    background-color: aliceblue;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import installElementPlus from './plugins/element'
 
 Vue.config.productionTip = false
 
