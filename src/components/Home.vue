933ff511baafb6539de56c8385643312071320d4
xxc <2331896806@qq.com>
2021-02-07 00:40:56 +0800

    实现home页主体布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <!-- 头部区域 -->
+        <el-header>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <!-- 主体区域 -->
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside width="200px">Aside</el-aside>
+            <!-- 主体内容 -->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -16,5 +26,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container {
+    height: 100%;
+}
+.el-header {
+    background-color: #373d41;
+}
 
+.el-aside {
+    background-color: #333744;
+}
+
+.el-main {
+    background-color: #eaecf1;
+}
 </style>
