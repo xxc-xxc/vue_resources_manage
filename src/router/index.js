84da2d923e0ecd326a9f240d81713c87f5168efe
xxc <2331896806@qq.com>
2021-02-06 23:48:28 +0800

    实现路由导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -15,4 +15,16 @@ const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  // next() 放行 next('/login') 强制跳转
+  if (to.path === '/login') return next();
+  const tokenStr = sessionStorage.getItem("token");
+  if (!tokenStr) return next('/login');
+  next();
+})
+
 export default router
