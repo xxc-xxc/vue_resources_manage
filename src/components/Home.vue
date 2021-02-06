c12f2f76e392c55b65c92371aea0abd3deec5909
xxc <2331896806@qq.com>
2021-02-07 00:28:21 +0800

    实现退出功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        Home组件
+        <el-button type="info" @click="logout">退出</el-button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        logout() {
+            sessionStorage.clear(); // 清空token
+            this.$router.push("/login"); // 跳转到登录页
+        }
+    }
 }
 </script>
 
