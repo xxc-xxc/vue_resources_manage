1440fc7917ecba40f07a81a4c49c98c725de882c
xxc <2331896806@qq.com>
2021-03-03 21:06:52 +0800

    实现展示分配权限对话框&获取所有权限数据

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -21,15 +21,28 @@
                 <!-- 展开列 -->
                 <el-table-column type="expand">
                     <template v-slot="scope">
-                        <el-row v-for="(item1, i1) in scope.row.children" :key="item1.id">
+                        <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
                             <!-- 渲染一级权限 -->
                             <el-col :span="5">
-                                <el-tag>{{item1.authName}}</el-tag>
+                                <el-tag closable @close="removeRightById(scope.row, item1.id)">{{item1.authName}}</el-tag>
+                                <i class="el-icon-caret-right"></i>
                             </el-col>
                             <!-- 渲染二级&三级权限 -->
-                            <el-col :span="19"></el-col>
+                            <el-col :span="19">
+                                <!-- 渲染二级权限 -->
+                                <el-row :class="[i2 === 0 ? '' : 'bdtop', 'vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                                    <el-col :span="6">
+                                        <el-tag closable @close="removeRightById(scope.row, item2.id)" type="success">{{item2.authName}}</el-tag>
+                                        <i class="el-icon-caret-right"></i>
+                                    </el-col>
+
+                                    <el-col :span="18">
+                                        <el-tag closable @close="removeRightById(scope.row, item3.id)" type="warning" v-for="(item3, i3) in item2.children" :key="item3.id">{{item3.authName}}</el-tag>
+                                    </el-col>
+                                </el-row>
+                            </el-col>
                         </el-row>
-                        <pre>{{scope.row}}</pre>
+                        <!-- <pre>{{scope.row}}</pre> -->
                     </template>
                 </el-table-column>
                 <!-- 索引列 -->
@@ -37,14 +50,26 @@
                 <el-table-column prop="roleName" label="角色名称"></el-table-column>
                 <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
                 <el-table-column label="操作">
-                    <template v-slot="scope">
+                    <template>
                         <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
                         <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
-                        <el-button type="warning" icon="el-icon-setting" size="mini">分配权限</el-button>
+                        <el-button type="warning" icon="el-icon-setting" size="mini" @click="showSetRightDialog">分配权限</el-button>
                     </template>
                 </el-table-column>
             </el-table>
         </el-card>
+
+        <!-- 分配权限对话框 -->
+        <el-dialog
+            title="分配权限"
+            :visible.sync="setRightDialogVisible"
+            width="50%">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="setRightDialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="setRightDialogVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -53,7 +78,11 @@ export default {
     data() {
         return {
             // 所有角色列表数据
-            rolelist: []
+            rolelist: [],
+            // 控制分配权限对话框的显示与隐藏
+            setRightDialogVisible: false,
+            // 保存权限数据
+            rightslist: []
         }
     },
     created() {
@@ -68,11 +97,64 @@ export default {
 
             this.rolelist = res.data;
             // console.log(res.data);
+        },
+        async removeRightById(role, rightId) {
+            const confirmResult = await this.$confirm('此操作将永久删除该权限, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).catch(err => err);
+
+            if(confirmResult !== 'confirm') {
+                return this.$message.info('取消删除');
+            }
+            // console.log('确认删除');
+            // 发起删除权限请求
+            const {data: res} = await this.$http.delete(`roles/${role.id}/rights/${rightId}`);
+
+            // 删除权限失败
+            if(res.meta.status !== 200) {
+                return this.$message.error('删除权限失败！');
+            }
+            // this.getRoleList(); // 会刷新页面，体验不好
+            role.children = res.data; // 直接将删除后的结果赋值给children，不会刷新页面
+        },
+        // 展示分配权限对话框
+        async showSetRightDialog() {
+            // 获取所有权限数据
+            const {data: res} = await this.$http.get('rights/tree');
+
+            // 获取数据失败
+            if(res.meta.status !== 200) {
+                return this.$message.error('获取权限数据失败！');
+            }
+
+            // 把获取到的权限数据保存到data中
+            this.rightslist = res.data;
+            console.log(this.rightslist);
+
+            this.setRightDialogVisible = true;
         }
     },
 }
 </script>
 
 <style lang="less" scoped>
+.el-tag {
+    margin: 10px;
+}
 
+.bdtop {
+    border-top: 1px solid #eee;
+}
+
+.bdbottom {
+    border-bottom: 1px solid #eee;
+}
+
+// 垂直居中
+.vcenter {
+    display: flex;
+    align-items: center;
+}
 </style>
