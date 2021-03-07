1e2cd1ece786eb4f928b7471a5870803e07e20fb
xxc <2331896806@qq.com>
2021-03-07 14:49:21 +0800

    完成商品分类功能开发

--- src/components/goods/Category.vue
+++ src/components/goods/Category.vue
@@ -62,21 +62,30 @@
         <el-dialog
             title="添加分类"
             :visible.sync="addCateDialogVisible"
-            width="50%">
+            width="50%"
+            @close="addCateDialogClosed">
             <el-form 
                 :model="addCateForm" 
                 :rules="addCateFormRules" 
                 ref="addCateFormRef" 
                 label-width="100px">
-                <el-form-item label="分类名称:" prop="cate_name">
-                    <el-input v-model="addCateForm.cate_name"></el-input>
+                <el-form-item label="分类名称:" prop="cat_name">
+                    <el-input v-model="addCateForm.cat_name"></el-input>
                 </el-form-item>
                 <el-form-item label="父级分类:">
+                    <el-cascader
+                        :options="parentCateList"
+                        :props="cascaderProps"
+                        v-model="selectedKeys"
+                        @change="parentCateChanged"
+                        clearable
+                        change-on-select>
+                    </el-cascader>
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="addCateDialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="addCateDialogVisible = false">确 定</el-button>
+                <el-button type="primary" @click="addCate">确 定</el-button>
             </span>
         </el-dialog>
     </div>
@@ -136,10 +145,21 @@ export default {
             },
             // 添加分类表单验证规则对象
             addCateFormRules: {
-                cate_name: [
+                cat_name: [
                     {required: true, message: '请输入分类名称', trigger: 'blur'}
                 ]
-            }
+            },
+            // 父级分类数据列表
+            parentCateList: [],
+            // 指定级联选择器的配置对象
+            cascaderProps: {
+                expandTrigger: 'hover',
+                value: 'cat_id',
+                label: 'cat_name',
+                children: 'children'
+            },
+            // 选中的父级分类的id数组
+            selectedKeys: []
         }
     },
     created() {
@@ -170,7 +190,61 @@ export default {
         },
         // 显示添加分类对话框
         showAddCateDialog() {
+            this.getParentCateList();
             this.addCateDialogVisible = true;
+        },
+        // 获取父级分类列表数据
+        async getParentCateList() {
+            const {data: res} = await this.$http.get('categories', {
+                params: {type: 2}
+            });
+
+            // 获取父级分类数据失败
+            if(res.meta.status !== 200) {
+                return this.$message.error('获取父级分类数据失败！');
+            }
+
+            // console.log(res.data);
+            this.parentCateList = res.data;
+            console.log(this.parentCateList)
+        },
+        // 选择项发生变化时触发
+        parentCateChanged() {
+            // console.log(this.selectedKeys);
+            // 如果selectedKeys数组中的length大于0，证明选中了父级分类
+            // 反之，说明没有选中父级分类
+            if(this.selectedKeys.length > 0) {
+                // 父级分类id
+                this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1];
+                // 为当前分类等级赋值
+                this.addCateForm.cat_level = this.selectedKeys.length;
+            } else {
+                // 没有选择父级分类
+                this.addCateForm.cat_pid = 0;
+                this.addCateForm.cat_level = 0;
+            }
+        },
+        // 点击确定添加分类
+        addCate() {
+            // console.log(this.addCateForm);
+            // 表单预校验
+            this.$refs.addCateFormRef.validate(async valid => {
+                if(!valid) return
+                const {data: res} = await this.$http.post('categories', this.addCateForm);
+                if(res.meta.status !== 201) {
+                    return this.$message.error('添加分类失败！');
+                }
+                this.$message.success('添加分类成功');
+                this.getCateList();
+                this.addCateDialogVisible = false;
+            })
+        },
+        // 对话框关闭 重置表单
+        addCateDialogClosed() {
+            this.$refs.addCateFormRef.resetFields();
+            this.selectedKeys = [];
+            this.addCateForm.cat_pid = 0;
+            this.addCateForm.cat_level = 0;
         }
     },
 }
@@ -179,5 +253,8 @@ export default {
 <style lang="less" scoped>
 .tree-table {
     margin-top: 15px;
+}
+.el-cascader {
+    width: 100%;
 }
 </style>
